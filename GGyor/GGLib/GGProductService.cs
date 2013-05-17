c829717dab609832b26560de6c206c986d54259a
Ertan Yavuz <yavuzze@gmail.com>
2013-05-17 18:51:03 +0300

    product service

--- GGyor/GGLib/GGProductService.cs
+++ GGyor/GGLib/GGProductService.cs
@@ -28,9 +28,89 @@ namespace GGLib
         {
             var prodService = ServiceProvider.getProductService();
             var response = prodService.getProducts(0, 100, "L", true, "tr");
-            var products = response.products.Select(x => x.product.description);
+            var products = response.products.Select(x => x.product.title).ToList();
 
             products.GetType();
+
+            var product = response.products[0];
+
+            var str = "";
+            foreach (var pr in response.products)
+            {
+                str += pr.itemId + "\t";
+                str += pr.product.title + "\t";
+                str += pr.product.affiliateOption.ToString() + "\t";
+                str += pr.product.boldOption.ToString() + "\t";
+                str += pr.product.catalogOption.ToString() + "\t";
+                str += pr.product.vitrineOption.ToString() + "\t";
+                str += pr.product.buyNowPrice.ToString() + "\t";
+                str += pr.product.startPrice.ToString() + "\t";
+                str += pr.product.cargoDetail.cargoCompanies[0] + "\t";
+
+                str += "\n";
+            }
+
+                                 
+            Console.WriteLine();
+        }
+
+        public productType CreateProduct()
+        {
+            var prodService = ServiceProvider.getProductService();
+
+            var product = new productType();
+            product.categoryCode = "a";
+            product.title = "Test Ürünü";
+
+            product.specs = new[]
+                {
+                    new specType {name = "Marka", type = "Combo", value = "Patates", required = true},
+                    new specType {name = "Durumu", type = "Combo", value = "Sıfır", required = true},
+                    new specType {name = "Garanti", type = "Combo", value = "Yok", required = true}
+                };
+
+            product.photos = new[]
+                {
+                    new photoType {photoId = 0, photoIdSpecified = true, url = "http://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Bamberger_Hoernle.jpg/320px-Bamberger_Hoernle.jpg"},
+                    new photoType {photoId = 1, photoIdSpecified = true, url = "http://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Russet_potato_.jpg/320px-Russet_potato_.jpg"}
+                };
+
+            product.pageTemplate = 1;
+            product.pageTemplateSpecified = true;
+            product.description = "Patates (Solanum tuberosum), patlıcangiller (Solanaceae) familyasından yumruları yenen otsu bitki türü.";
+            product.format = "A";
+            product.startPrice = 0;
+            product.startPriceSpecified = true;
+            product.buyNowPrice = 5.00;
+            product.buyNowPriceSpecified = true;
+            product.listingDays = 1;
+            product.listingDaysSpecified = true;
+            product.productCount = 1;
+            product.productCountSpecified = true;
+
+            product.cargoDetail = new cargoDetailType
+                {
+                    city = "20",
+                    cargoCompanies = new[] {"Aras", "Yurtiçi"},
+                    cargoDescription = "Kargo alıcıya aittir",
+                    shippingPayment = "B",
+                    shippingWhere = "Turkey"
+                };
+
+            product.affiliateOption = false;
+            product.boldOption = false;
+            product.catalogOption = true;
+            product.vitrineOption = false;
+            product.startDate = "17.05.2013 00:00:00";
+
+            var response = prodService.insertProduct("ptt001", product, false, false, "tr");
+            if (response != null && response.ackCode == "success")
+            {
+                var id = response.productId;
+                return product;
+            }
+
+            return null;
 
         }
 
