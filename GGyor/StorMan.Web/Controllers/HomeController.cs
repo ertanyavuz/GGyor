1798ab3554b3fc7697936b37cdcc9d11f5fb09bc
Ertan Yavuz <yavuzze@gmail.com>
2013-06-17 18:33:02 +0300

    Category parent-child mappings

--- GGyor/StorMan.Web/Controllers/HomeController.cs
+++ GGyor/StorMan.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using StorMan.Business;
+using StorMan.Model;
 
 namespace StorMan.Web.Controllers
 {
@@ -31,6 +32,20 @@ namespace StorMan.Web.Controllers
                     catTable[x.Code].LocalCategoryModel = x;
                 }
             });
+
+            foreach (var code in catTable.Keys)
+            {
+                var category = catTable[code];
+                if (!String.IsNullOrWhiteSpace(code) && !String.IsNullOrWhiteSpace(code.Substring(0, code.Length - 1)))
+                {
+                    var parentCode = code.Substring(0, code.Length - 1);
+                    if (catTable.ContainsKey(parentCode))
+                        category.Parent = catTable[parentCode];
+                }
+                if (category.Children == null)
+                    category.Children = new List<CategoryModel>();
+                category.Children.AddRange(catTable.Values.Where(x => x.Code.StartsWith(code)).ToList());
+            }
 
             ViewBag.LocalCategories = list;
             ViewBag.CategoryTable = catTable;
