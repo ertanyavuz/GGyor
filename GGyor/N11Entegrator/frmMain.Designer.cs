50d1297efe69d184ab7ed6853080026fa24e9bcf
ertanyavuz <yavuzze@gmail.com>
2014-11-28 03:34:18 +0200

    New Entegrator

---
+++ GGyor/N11Entegrator/frmMain.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace N11Entegrator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 565);
+            this.Name = "frmMain";
+            this.Text = "N11 Entegrator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
